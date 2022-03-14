1e14012e55f4e1c085f911f4d1117506d4bc1491
John U <arjorb@gmail.com>
2022-03-14 19:28:05 +0200

    Movie App Done

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 const APIKEY = '04c35731a5ee918f014970082a0088b1';
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-getMovies();
-async function getMovies(){
-    const resp = await fetch(APIURL);
-    const respDate = await resp.json();
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-    respDate.results.forEach(movie =>{
+const main = document.querySelector('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+getMovies(APIURL);
+async function getMovies(url){
+    const resp = await fetch(url);
+    const respDate = await resp.json();
+    showMovies(respDate.results);
+
+    
+}
+
+
+function getColorRating(vote){
+    if(vote >= 8){
+        return 'green'
+    }else if(vote > 6){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+}
+
+function showMovies(movies){
+    main.innerHTML =''
+    movies.forEach(movie =>{
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
 
         movieEl.innerHTML = `
         <img src="${IMGPATH + movie.poster_path}" alt="${movie.title}">
         <div class="movie-info">
-            <h3>${movie.title}</h3>
-            <span class=${getColorRating(movie.vote_average)}>${movie.vote_average}</span>
+            <h3>
+            ${movie.title}
+            </h3>
+            <span class=${getColorRating(movie.vote_average)}>
+            ${movie.vote_average}
+            </span>
+        </div>
+        <div class="overview">
+        <h3>Overview:</h3>
+        ${movie.overview}
         </div>
         `
-
-        document.querySelector('main').appendChild(movieEl);
+        main.appendChild(movieEl);
     })
 }
 
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    let searchTerm = search.value;
 
-function getColorRating(vote){
-    if(vote > 7){
-        return 'green'
-    }else if(vote > 6){
-        return 'oragnge'
-    }else{
-        return 'red'
+    if(searchTerm){
+
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value= '';
     }
-}
+})
